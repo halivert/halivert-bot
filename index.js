bacc8c584bd4c40a65295d1455cae5bb90fa5d89
Hali V <halivelazquez@outlook.com>
2020-04-05 05:04:45 -0500

    Add webhook to bot

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Telegraf = require("telegraf");
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const PORT = process.env.PORT || 5000;
+const BOT_PORT = process.env.BOT_PORT || 3000;
+const URL = process.env.URL || "http://halivert-bot.herokuapp.com";
+
+const bot = new Telegraf(BOT_TOKEN);
 bot.start(ctx => ctx.reply("Welcome"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
@@ -13,7 +17,12 @@ bot.command("oldschool", ctx => ctx.reply("Hello"));
 bot.command("modern", ({ reply }) => reply("Yo"));
 bot.command("hipster", Telegraf.reply("λ"));
 
-bot.launch();
+bot.launch({
+	webhook: {
+		domain: `${URL}/${BOT_TOKEN}`,
+		port: BOT_PORT
+	}
+});
 
 express()
 	.use(express.static(path.join(__dirname, "public")))
@@ -22,4 +31,6 @@ express()
 		res.status(400);
 		res.sendFile(path.join(__dirname, "public/404.html"));
 	})
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+	.listen(PORT, () => {
+		console.log(`Listening on ${PORT}`);
+	});

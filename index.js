6113612ad1d130a5c4af8fffd225147d310ffcab
Hali V <halivelazquez@outlook.com>
2020-04-05 06:02:06 -0500

    Please read documentation of the frameworks...

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const PORT = process.env.PORT || 5000;
-const BOT_PORT = process.env.BOT_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || "https://halivert-bot.herokuapp.com";
 
 const bot = new Telegraf(BOT_TOKEN);
@@ -17,24 +16,13 @@ bot.command("oldschool", ctx => ctx.reply("Hello"));
 bot.command("modern", ({ reply }) => reply("Yo"));
 bot.command("hipster", Telegraf.reply("λ"));
 
-const unless = function(middleware, ...paths) {
-	return function(req, res, next) {
-		const pathCheck = paths.some(path => path === req.path);
-		pathCheck ? next() : middleware(req, res, next);
-	};
-};
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
 
-// express()
-// 	// .use('/', express.static(path.join(__dirname, "public")))
-// 	// .use(function(req, res) {
-// 	// 	res.status(404);
-// 	// 	res.sendFile(path.join(__dirname, "public/404.html"));
-// 	// })
-// 	.listen(PORT);
-
-bot.launch({
-	webhook: {
-		domain: URL,
-		port: BOT_PORT
-	}
-});
+express()
+	.use(bot.webhookCallback(`/bot${BOT_TOKEN}`))
+	.use(express.static(path.join(__dirname, "public")))
+	.use(function(req, res) {
+		res.status(404);
+		res.sendFile(path.join(__dirname, "public/404.html"));
+	})
+	.listen(PORT);

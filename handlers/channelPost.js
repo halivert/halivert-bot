a86fa2a446503363cc0dcd3431137bc007ae0182
Halí V <halivelazquez@outlook.com>
2020-05-23 18:47:04 -0500

    Update comments button with login

---
+++ handlers/channelPost.js
@@ -0,0 +1,50 @@
+const Markup = require("telegraf/markup");
+const Extra = require("telegraf/extra");
+const URL = require("url-parse");
+
+module.exports = bot => {
+	bot.on("channel_post", async ctx => {
+		var url;
+		const channelPost = ctx.channelPost;
+		const chatId = channelPost.chat.id;
+		const messageId = channelPost.message_id;
+		if (channelPost.entities && channelPost.entities.length === 1) {
+			const entity = channelPost.entities[0];
+			if (entity.type === "url") {
+				url = new URL(ctx.channelPost.text, true);
+			}
+		}
+
+		if (!url) return;
+		if (url.hostname !== "t.me") return;
+		if (url.pathname !== "/iv") return;
+
+		const buttonUrl = url.query.url + "#comments";
+
+		ctx.telegram.editMessageReplyMarkup(
+			chatId,
+			messageId,
+			null,
+			Markup.inlineKeyboard([
+				Markup.loginButton("Comments", buttonUrl)
+			])
+		);
+			// reply_markup: Extra.markup(m => {
+			// 	m.inlineKeyboard([m.urlButton("Comments", buttonUrl)]);
+			// })
+		// });
+
+		// if (ctx.channelPost.text.toLowerCase() === `@${ctx.me}`.toLowerCase()) {
+		// 	const { title, username } = ctx.chat;
+
+		// 	const reply = await ctx.reply(
+		// 		`Success. This message will automatically be deleted in 5 seconds.`
+		// 	);
+		// 	ctx.deleteMessage();
+
+		// 	setTimeout(() => {
+		// 		ctx.deleteMessage(reply.message_id);
+		// 	}, 5000);
+		// }
+	});
+};
--- index.js
+++ index.js
@@ -2,27 +2,50 @@ const Telegraf = require("telegraf");
 const express = require("express");
 const path = require("path");
 
+const handlers = ["channelPost"];
+
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || "https://halivert-bot.herokuapp.com";
 
-const bot = new Telegraf(BOT_TOKEN);
+const bot = new Telegraf(BOT_TOKEN, {
+	channelMode: false
+});
+
 bot.start(ctx => ctx.reply("Welcome"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
 bot.on("sticker", ctx => ctx.reply("👍"));
 bot.hears("hi", ctx => ctx.reply("Hey there"));
 
+handlers.forEach(handler => require(`./handlers/${handler}`)(bot));
+
 bot.command("oldschool", ctx => ctx.reply("Hello"));
 bot.command("modern", ({ reply }) => reply("Yo"));
 bot.command("hipster", Telegraf.reply("λ"));
 
-bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+const app = express();
 
-express()
-	.use(bot.webhookCallback(`/bot${BOT_TOKEN}`))
+if (process.env.APP_ENV === "local") bot.launch();
+else {
+	bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+	app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
+}
+
+app
 	.use(express.static(path.join(__dirname, "public")))
 	.use(function(req, res) {
 		res.status(404);
 		res.sendFile(path.join(__dirname, "public/404.html"));
 	})
 	.listen(PORT);
+
+// bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+
+// express()
+// 	.use(bot.webhookCallback(`/bot${BOT_TOKEN}`))
+// 	.use(express.static(path.join(__dirname, "public")))
+// 	.use(function(req, res) {
+// 		res.status(404);
+// 		res.sendFile(path.join(__dirname, "public/404.html"));
+// 	})
+// 	.listen(PORT);
